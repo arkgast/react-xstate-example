d3f38e33da29f4355a7965176bed0145df3f2ca7
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-09-29 07:33:53 -0400

    feat: design states

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Machine } from 'xstate'
+import { useMachine } from '@xstate/react'
+
+const stateMachine = Machine({
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        SUBMIT: 'loading'
+      }
+    },
+    loading: {
+      on: {
+        PAYMENT_RECEIVED: 'success',
+        PAYMENT_FAILED: 'error'
+      }
+    },
+    error: {
+      on: {
+        SUBMIT: 'loading'
+      }
+    },
+    success: {
+      type: 'final'
+    }
+  }
+})
 
 const App = () => {
+  const [machine, send] = useMachine(stateMachine)
+
+  console.log(machine.value)
+
   return (
-    <h1>React State Machine</h1>
+    <div>
+      <h1>React State Machine</h1>
+      <button onClick={() => send({ type: 'SUBMIT', data: {} })}>Change state</button>
+    </div>
   )
 }
 
